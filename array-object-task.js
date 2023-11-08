5adbddf322008fafeae6d80f17520ce71335a1af
Bolormaa <135795277+bolourima@users.noreply.github.com>
2023-11-08 19:27:49 +0800

    array reverse

--- array-object-task.js
+++ array-object-task.js
@@ -5,35 +5,34 @@
 //   b: 2,
 //   a: 1,
 //   c: 3,
-//   f: 6,
-//   l: 4,
-//   s: 8,
 // };
-// console.log(Object.keys(order))
-// for(i = 0; i < order.keys; i++){
-//     for(j = i; j < order.keys; j++){
-//         if(order.keys[i] < order.keys[j]){
-//             let x = order.keys[i]
-//             order.keys[i] = order.keys[j]
-//             order.keys[j] = x
-//         }
-//     }
-// } console.log(keys)
+// const unorder = Object.entries(order);
+// for(let i = 0; i < unorder.length; i++){
+//     for(let u = 0; u < unorder.length; u ++)
+//     if(unorder[i][1] < unorder[u][1]){
+//     let x = unorder[i];
+//     unorder[i] = unorder[u];
+//     unorder[u] = x;
+// }
+// }
+// console.log(Object.entries(unorder))
 
-const order = {
-  b: 2,
-  a: 1,
-  c: 3,
-};
-const unorder = Object.entries(order);
-for(let i = 0; i < unorder.length; i++){
-    for(let u = 0; u < unorder.length; u ++)
-    if(unorder[i][1] < unorder[u][1]){
-    let x = unorder[i];
-    unorder[i] = unorder[u];
-    unorder[u] = x;
+// Array reverse
+
+let fullName = [
+{
+  lastName: 'hi',
+  firstName: 'henee'
+} 
+]
+
+{
+  if (lastName && firstName) {
+    const upperLastName = lastName.toUpperCase();
+    return `${upperLastName + ' ' + firstName} `;
+  }
+  return 'Буруу утга оруулсан байна.';
 }
-}
-console.log(Object.entries(unorder))
+
 
 
