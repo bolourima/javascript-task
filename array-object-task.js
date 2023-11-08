46b4bf9ca3582234aa2e02a5d13537c5adc29343
Bolormaa <135795277+bolourima@users.noreply.github.com>
2023-11-08 19:10:53 +0800

    11-08 task

--- array-object-task.js
+++ array-object-task.js
@@ -1,29 +1,39 @@
 // Array-object-string-method TASKs
 
 //Ascending order
+// const order = {
+//   b: 2,
+//   a: 1,
+//   c: 3,
+//   f: 6,
+//   l: 4,
+//   s: 8,
+// };
+// console.log(Object.keys(order))
+// for(i = 0; i < order.keys; i++){
+//     for(j = i; j < order.keys; j++){
+//         if(order.keys[i] < order.keys[j]){
+//             let x = order.keys[i]
+//             order.keys[i] = order.keys[j]
+//             order.keys[j] = x
+//         }
+//     }
+// } console.log(keys)
+
 const order = {
   b: 2,
   a: 1,
   c: 3,
-  f: 6,
-  l: 4,
-  s: 8,
 };
-// console.log(order)
-for (const [key, value] of Object.entries(order)) {
-  if (value > value + 1) {
-    console.log(value);
-  }
+const unorder = Object.entries(order);
+for(let i = 0; i < unorder.length; i++){
+    for(let u = 0; u < unorder.length; u ++)
+    if(unorder[i][1] < unorder[u][1]){
+    let x = unorder[i];
+    unorder[i] = unorder[u];
+    unorder[u] = x;
 }
-
-// for(i = 0; i < order.length; i++){
-//     for(j = i; j < order.length; j++){
-//         if(order[i] < order[j]){
-//             let x = order[i]
-//             order[i] = order[j]
-//             order[j] = x
-//         }
-//     }
-// }
+}
+console.log(Object.entries(unorder))
 
 
